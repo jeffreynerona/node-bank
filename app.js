3554671f564aea07c55ee3f8b22532a1dbb4efee
jeffreynerona <jeffnerona@gmail.com>
2017-09-06 00:04:14 +0800

    Update mongodb for heroku deployment

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var api = require('./routes/api');
 var app = express();
 redirect(app);
 //Connect to Mongoose
-mongoose.connect('mongodb://localhost/nodebank');
+mongoose.connect('mongodb://jeffreynerona:VyxfpsiDdWh4oF1I@cluster0-shard-00-00-k7flg.mongodb.net:27017,cluster0-shard-00-01-k7flg.mongodb.net:27017,cluster0-shard-00-02-k7flg.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
 var db = mongoose.connection;
 
 // view engine setup
